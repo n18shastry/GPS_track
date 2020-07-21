a49b1326dd3c042562c3845fb8b775212c45a350
NISHANTH SHASTRY <41582676+n18shastry@users.noreply.github.com>
2020-07-21 11:20:43 +0530

    Update ipfind.py

--- RaspberryPi/ipfind.py
+++ RaspberryPi/ipfind.py
@@ -2,7 +2,7 @@ import ipinfo
 from requests import get
 
 
-access_token = 'ee07ca180b3e5a'
+access_token = 'API_KEY'
 ip = get('https://api.ipify.org').text
 print ('My public IP address is:', ip)
 handler = ipinfo.getHandler(access_token)
